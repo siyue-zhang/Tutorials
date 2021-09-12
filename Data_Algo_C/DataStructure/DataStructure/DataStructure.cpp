ed33cd03948c4b0eb3db4bf3587a21ad9e1a9388
siyue-zhang <siyue.zhang.sg@gmail.com>
2021-09-12 17:01:52 +0800

    MLOps

---
+++ Data_Algo_C/DataStructure/DataStructure/DataStructure.cpp
@@ -0,0 +1,25 @@
+
+#include <iostream>
+#include <stdio.h> // for cpp
+
+using namespace std;
+
+int main()
+{
+	int A[10] = {2,4,5,6,12};
+
+	cout<<sizeof(A)<<endl; // A[5] size is 20, 4 bytes per number
+	// endl move to the next line
+
+	cout <<A[1]<<"\n"; // cout is for cpp
+	printf("%d\n", A[8]); // printf is for c
+
+	int B[5] = { 1,2,3,4,66};
+
+	for (int x:B)
+	{
+		cout << x << endl;
+	}
+
+	return 0;
+}
